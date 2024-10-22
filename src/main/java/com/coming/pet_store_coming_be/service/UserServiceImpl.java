4d7aa62b117cbfa4e76d129030579fcbf031aa4e
JGW-Korea <jgw6372@gmail.com>
2024-10-22 15:36:36 +0900

    feat: Spring Security Config 생성

--- src/main/java/com/coming/pet_store_coming_be/service/UserServiceImpl.java
+++ src/main/java/com/coming/pet_store_coming_be/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.coming.pet_store_coming_be.dao.UserDAO;
@@ -16,26 +18,34 @@ import com.coming.pet_store_coming_be.dto.UserIdentifierDTO;
 public class UserServiceImpl implements UserService {
 
   @Autowired
-  UserIdentifierDAO identifierDAO;
+  private UserIdentifierDAO identifierDAO;
 
   @Autowired
-  UserDAO userDAO;
+  private UserDAO userDAO;
 
   @Override // 회원가입 Service
   public boolean signUpUser(UserDTO user) throws SQLException{
+
+    System.out.println("Nice Meet You");
 
     UUID uuid4 = UUID.randomUUID(); // 사용자의 고유 번호 생성
 
     // 1. User Identifier 테이블에 현재 회원가입을 시도하려는 사용자의 고유 번호 등록
     UserIdentifierDTO identifier = new UserIdentifierDTO(uuid4, LocalDateTime.now(), LocalDateTime.now());
 
-    // 고유 번호 등록을 성공할 경우
+    // 2. 고유 번호 등록을 성공할 경우 -> 회원가입 진행
     if(identifierDAO.createUserIdentifier(identifier) > 0) {
       user.setUserIdentifierId(uuid4); // 사용자의 기본키 및 외래키에 UUID 값 적용
 
       // 비밀번호 암호화
+      BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+      user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+      // 회원가입 DAO 수행
+
+      // System.out.println(passwordEncoder.encode(user.getPassword()).toString());
       
-    }
+    } 
 
     return false;
   }
