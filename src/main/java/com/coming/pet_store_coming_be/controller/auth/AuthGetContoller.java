0d8214c8456a396be3581dcfd04a5bde79f9e06c
JGW-Korea <jgw6372@gmail.com>
2024-10-28 21:50:22 +0900

    feat: 중복 로그인 방지 기능 구현 시작 (#7)

--- src/main/java/com/coming/pet_store_coming_be/controller/auth/AuthGetContoller.java
+++ src/main/java/com/coming/pet_store_coming_be/controller/auth/AuthGetContoller.java
@@ -30,7 +30,9 @@ public class AuthGetContoller {
   TokenProvider tokenProvider;
 
   @GetMapping("/sign-in")
-  public ResponseEntity<Map<String, Object>> getLoginUser(@RequestParam("email") String email, @RequestParam("password") String password) throws SQLException {
+  public ResponseEntity<Map<String, Object>> getLoginUser(@RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("deviceId") String deviceId) throws SQLException {
+    
+    
     Map<String, Object>  response = new HashMap<>();
     
     // 입력으로 주어진 email를 통해 DB에서 해당 데이터를 사용자 정보를 가져온다.
@@ -56,30 +58,32 @@ public class AuthGetContoller {
       return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 
-    // 3. 중복 로그인을 시도할 경우
-    else if(userInfo.getIsActive()) {
-      response.put("status", HttpStatus.CONFLICT.value());
-      response.put("success", false);
-      response.put("message", "You are already logged in on another device.");
-      response.put("errorCode", "DUPLICATE_LOGIN_ATTEMPT");
 
-      return new ResponseEntity<>(response, HttpStatus.CONFLICT);
-    }
 
-    // 모든 경우에 걸리지 않았을 경우 - 토큰 생성
-    String token = tokenProvider.createToken(userInfo);
+    // // 3. 중복 로그인을 시도할 경우
+    // else if(userInfo.getIsActive()) {
+    //   response.put("status", HttpStatus.CONFLICT.value());
+    //   response.put("success", false);
+    //   response.put("message", "You are already logged in on another device.");
+    //   response.put("errorCode", "DUPLICATE_LOGIN_ATTEMPT");
 
-    // 로그인 성공 시 refresh_token 및 is_active 갱신
-    userInfo.setRefreshToken(tokenProvider.createRefreshToken(userInfo.getId()));
-    userInfo.setIsActive(true);
+    //   return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    // }
 
-    authService.refreshTokenAndExpiry(userInfo.getId(), userInfo.getRefreshToken(), userInfo.getIsActive());
+    // // 모든 경우에 걸리지 않았을 경우 - 토큰 생성
+    // String token = tokenProvider.createToken(userInfo);
+
+    // // 로그인 성공 시 refresh_token 및 is_active 갱신
+    // userInfo.setRefreshToken(tokenProvider.createRefreshToken(userInfo.getId()));
+    // userInfo.setIsActive(true);
+
+    // authService.refreshTokenAndExpiry(userInfo.getId(), userInfo.getRefreshToken(), userInfo.getIsActive());
 
     // 토큰 생성 및 UserInfo refresh_token 및 is_active 갱신을 완료한 이후 응답 생성
     response.put("status", HttpStatus.OK.value());
     response.put("success", true);
     response.put("message", "Login successful.");
-    response.put("token", token);
+    // response.put("token", token);
 
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
