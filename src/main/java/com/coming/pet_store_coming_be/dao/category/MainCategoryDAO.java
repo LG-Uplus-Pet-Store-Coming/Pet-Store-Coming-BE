a9db7b1125a5219929f356cfe52825372cce899b
JGW-Korea <jgw6372@gmail.com>
2024-10-30 21:27:00 +0900

    feat: Main Category DAO findAllMainCategoryInfo() 메서드 정의 (#10)

---
+++ src/main/java/com/coming/pet_store_coming_be/dao/category/MainCategoryDAO.java
@@ -0,0 +1,15 @@
+package com.coming.pet_store_coming_be.dao.category;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.coming.pet_store_coming_be.dto.MainCategoryDTO;
+
+import java.util.List;
+import java.sql.SQLException;
+
+@Mapper
+public interface MainCategoryDAO {
+
+  public List<MainCategoryDTO> findAllMainCategoryInfo() throws SQLException; // 모든 메인 카테고리 정보 불러오기
+
+}
--- src/main/java/com/coming/pet_store_coming_be/service/category/main_category/MainCategoryServiceImpl.java
+++ src/main/java/com/coming/pet_store_coming_be/service/category/main_category/MainCategoryServiceImpl.java
@@ -1,15 +1,19 @@
 package com.coming.pet_store_coming_be.service.category.main_category;
 
+import com.coming.pet_store_coming_be.dao.category.MainCategoryDAO;
 import com.coming.pet_store_coming_be.dto.MainCategoryDTO;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainCategoryServiceImpl implements MainCategoryService {
+
+  @Autowired
+  MainCategoryDAO dao;
 
   @Override // 메인 카테고리 생성 비즈니스 로직 설계
   public boolean createMainCategory(MainCategoryDTO category) throws SQLException {
@@ -18,8 +22,7 @@ public class MainCategoryServiceImpl implements MainCategoryService {
 
   @Override // 메인 카테고리 리스트 가져오는 비즈니스 로직 설계
   public List<MainCategoryDTO> getMainCategoryList() throws SQLException {
-    
-    return new ArrayList<>();
+    return dao.findAllMainCategoryInfo();
   }
   
 }
