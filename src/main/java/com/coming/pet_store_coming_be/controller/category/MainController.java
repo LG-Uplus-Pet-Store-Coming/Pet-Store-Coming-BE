e798faebdbc0c7e9eb2b6737369965dc520aefb0
JGW-Korea <jgw6372@gmail.com>
2024-10-30 21:22:43 +0900

    feat: Main Category GET API 로직 작성 완료 (#10)

--- src/main/java/com/coming/pet_store_coming_be/controller/category/MainController.java
+++ src/main/java/com/coming/pet_store_coming_be/controller/category/MainController.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.coming.pet_store_coming_be.dto.MainCategoryDTO;
+import com.coming.pet_store_coming_be.service.category.main_category.MainCategoryService;
 
 
 @ResponseBody
 @RequestMapping("/category/main-category")
 public class MainController {
 
+  @Autowired
+  MainCategoryService mainCategoryService;
   
   // POST -> 메인 카테고리 생성
   @PostMapping("/create")
@@ -31,15 +37,29 @@ public class MainController {
 
   // GET -> 메인 카테고리 정보 가져오기
   @GetMapping("/list")
-  public ResponseEntity<Map<String, Object>> getMainCategoryList() {
+  public ResponseEntity<Map<String, Object>> getMainCategoryList() throws SQLException {
     Map<String, Object> response = new HashMap<>(); // 응답 결과
 
-    // 모든 메인 카테고리 정보 가져오기
-    List<MainCategoryDTO> categorys = new ArrayList<>();
-    // categorys;
 
+    try {
+      // 모든 메인 카테고리 정보 가져오기
+      List<MainCategoryDTO> categorys = mainCategoryService.getMainCategoryList();
 
-    return new ResponseEntity<>(response, HttpStatus.OK);
+      response.put("status", HttpStatus.OK.value());
+      response.put("success", true);
+      response.put("data", categorys);
+
+      return new ResponseEntity<>(response, HttpStatus.OK);  
+    } catch (Exception e) {
+
+      // 에러가 발생한 경우
+      response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+      response.put("success", false);
+      response.put("message", "Failed to load main category list.");
+      response.put("errorCode", "INTERNAL_SERVER_ERROR");
+
+      return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
 }
--- src/main/java/com/coming/pet_store_coming_be/service/category/main_category/MainCategoryServiceImpl.java
+++ src/main/java/com/coming/pet_store_coming_be/service/category/main_category/MainCategoryServiceImpl.java
@@ -1,8 +1,10 @@
 package com.coming.pet_store_coming_be.service.category.main_category;
 
 import com.coming.pet_store_coming_be.dto.MainCategoryDTO;
+
 import java.util.List;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 import org.springframework.stereotype.Service;
 
@@ -10,12 +12,12 @@ import org.springframework.stereotype.Service;
 public class MainCategoryServiceImpl implements MainCategoryService {
 
   @Override // 메인 카테고리 생성 비즈니스 로직 설계
-  public boolean createMainCategory(MainCategoryDTO category) {
+  public boolean createMainCategory(MainCategoryDTO category) throws SQLException {
     return false;
   }
 
   @Override // 메인 카테고리 리스트 가져오는 비즈니스 로직 설계
-  public List<MainCategoryDTO> getMainCategoryList() {
+  public List<MainCategoryDTO> getMainCategoryList() throws SQLException {
     
     return new ArrayList<>();
   }
