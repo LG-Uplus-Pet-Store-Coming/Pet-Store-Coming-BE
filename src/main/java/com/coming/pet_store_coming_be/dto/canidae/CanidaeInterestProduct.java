577d4f0f99db53fb463d0e1b0f1354383b234228
JGW-Korea <jgw6372@gmail.com>
2024-11-01 11:24:00 +0900

    feat: CanidaeInterestProductDTO 생성 (#14)

---
+++ src/main/java/com/coming/pet_store_coming_be/dto/canidae/CanidaeInterestProduct.java
@@ -0,0 +1,50 @@
+package com.coming.pet_store_coming_be.dto.canidae;
+
+public class CanidaeInterestProduct {
+  private String id;
+  private String canidaeId;
+  private String subCategoryId;
+
+  public CanidaeInterestProduct() {
+  }
+
+  public CanidaeInterestProduct(String id, String canidaeId, String subCategoryId) {
+    this.id = id;
+    this.canidaeId = canidaeId;
+    this.subCategoryId = subCategoryId;
+  }
+
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getCanidaeId() {
+    return this.canidaeId;
+  }
+
+  public void setCanidaeId(String canidaeId) {
+    this.canidaeId = canidaeId;
+  }
+
+  public String getSubCategoryId() {
+    return this.subCategoryId;
+  }
+
+  public void setSubCategoryId(String subCategoryId) {
+    this.subCategoryId = subCategoryId;
+  }
+
+  @Override
+  public String toString() {
+    return "{" +
+      " id='" + getId() + "'" +
+      ", canidaeId='" + getCanidaeId() + "'" +
+      ", subCategoryId='" + getSubCategoryId() + "'" +
+      "}";
+  }
+
+}
