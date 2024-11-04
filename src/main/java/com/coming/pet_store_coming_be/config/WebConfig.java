1994dfcbf5b97eb500be57fb9d19ba493fe896f2
JGW-Korea <jgw6372@gmail.com>
2024-11-04 14:08:48 +0900

    fix: 이미지 경로 수정

--- src/main/java/com/coming/pet_store_coming_be/config/WebConfig.java
+++ src/main/java/com/coming/pet_store_coming_be/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.coming.pet_store_coming_be.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,6 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
       .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
       .allowedHeaders("*") // 모든 헤더 허용 
       .allowCredentials(true); // 인증 정보를 허용할지 여부
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      // "/upload/**"로 시작하는 URL 요청이 실제 파일 시스템의 "file:./upload/" 경로로 매핑됩니다.
+      registry.addResourceHandler("/upload/**")
+              .addResourceLocations("file:///Users/jogyewon/Desktop/Web/Git/pet_store_coming_be/upload/");
   }
 
 }
