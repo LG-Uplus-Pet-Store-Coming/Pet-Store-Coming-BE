f670c3b1e32e875862d6c2cf37bc81ea3e5c4f66
JGW-Korea <jgw6372@gmail.com>
2024-11-07 13:08:30 +0900

    test: sysout 추가

--- src/main/java/com/coming/pet_store_coming_be/controller/product/ProductGetController.java
+++ src/main/java/com/coming/pet_store_coming_be/controller/product/ProductGetController.java
@@ -254,16 +254,24 @@ public class ProductGetController {
       // 사용자가 로그인 이후 메인 페이지 방문 시
       if(token != null && token.isBlank()) {
 
+        System.out.println("1. Hello");
+
         if(token.startsWith("Bearer ")) token = token.substring(7); // 토큰에 'Bearer' 이 포함되어 있을 경우 접두사 제거
         if(!tokenProvider.isTokenInvalid(token)) { // 토큰이 아직 유효할 경우
           String userId = tokenProvider.getUserIdFromToken(token);
 
+          System.out.println("2. token" + token);
+
           // 사용자 고유키를 가지고 온 경우
           if(userId != null) {
-          
+            System.out.println("3. User ID" + userId);
+
             // 사용자가 반려견을 등록한 경우 - 관심 상품 중 인기 상품
             if(canidaeService.getCanidaeListService(userId) != null) {
               List<ProductInfoDTO> canidaeInterstProduct = productService.getInterstProductService(userId);
+
+              System.out.println("*** Interset Product ***");
+              System.out.println(canidaeInterstProduct);
 
               response.put("status", HttpStatus.OK.value());
               response.put("success", true);
@@ -275,6 +283,9 @@ public class ProductGetController {
             // 사용자가 반려견을 등록하지 않은 경우 - 인기 상품
             else {
               List<ProductInfoDTO> popularProduct = productService.getPopularProductService();
+
+              System.out.println("** NO!!!! Popular Product **");
+              System.out.println(popularProduct);
 
               response.put("status", HttpStatus.OK.value());
               response.put("success", true);
@@ -288,6 +299,9 @@ public class ProductGetController {
 
       // 비로그인 사용자가 메인 페이지 접속 시
       List<ProductInfoDTO> popularProduct = productService.getPopularProductService();
+
+      System.out.println("** NO!!!! Popular Product **");
+      System.out.println(popularProduct);
 
       response.put("status", HttpStatus.OK.value());
       response.put("success", true);
