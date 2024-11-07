6d6f772ab31d500668c220c117c5b385a7bf3312
JGW-Korea <jgw6372@gmail.com>
2024-11-07 21:42:49 +0900

    CORS 도메인 추가

--- src/main/java/com/coming/pet_store_coming_be/config/WebConfig.java
+++ src/main/java/com/coming/pet_store_coming_be/config/WebConfig.java
@@ -14,8 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
+    String[] origins = allowedOrigin.split(",\\s*");
+    
     registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-      .allowedOrigins(allowedOrigin) // 허용할 출처
+      .allowedOrigins(origins) // 허용할 출처
       .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
       .allowedHeaders("*") // 모든 헤더 허용 
       .allowCredentials(true); // 인증 정보를 허용할지 여부
