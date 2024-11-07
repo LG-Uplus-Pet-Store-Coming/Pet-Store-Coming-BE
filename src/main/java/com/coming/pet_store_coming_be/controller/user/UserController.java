a56c658091a795a2cd323a8f65961ef3a5d10fd8
JGW-Korea <jgw6372@gmail.com>
2024-11-08 04:07:51 +0900

    fix HTTP Method

--- src/main/java/com/coming/pet_store_coming_be/controller/user/UserController.java
+++ src/main/java/com/coming/pet_store_coming_be/controller/user/UserController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/user")
@@ -105,7 +107,12 @@ public class UserController {
     
 
     // 회원정보 수정
-    @PostMapping("/")
+    @PutMapping("path/{id}")
+    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
+        //TODO: process PUT request
+        
+        return entity;
+    }("/info")
     public ResponseEntity<Map<String, Object>> updateUserInfoController(@RequestBody UserDTO user) {
         Map<String, Object> response = new HashMap<>();
         
